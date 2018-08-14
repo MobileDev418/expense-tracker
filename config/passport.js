42655d9e4ba333a888c3d9b68c5e25ac4c9cca53
Mobile Developer <oncemoreforyou@outlook.com>
2018-08-14 13:47:13 -0400

    added authorization fail request
    added authorization fail request

--- config/passport.js
+++ config/passport.js
@@ -53,3 +53,15 @@ exports.isAuthorized = (req, res, next) => {
     res.redirect(`/auth/${provider}`);
   }
 };
+/**
+ * Authorization Failed Required.
+ */
+exports.isFailAuthorized = (req, res, next) => {
+  const provider = req.path.split('/').slice(-1)[0];
+  const token = req.user.tokens.find(token => token.kind === provider);
+  if (token) {
+    next();
+  } else {
+    res.redirect(`/auth/${provider}`);
+  }
+};
